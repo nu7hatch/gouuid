691dbf0ad1303f30d5b4d2a0652a827733e5587a
Chris Kowalik <chris@nu7hat.ch>
2011-12-20 01:29:16 -0200

    fixed header comments

--- uuid.go
+++ uuid.go
@@ -1,21 +1,8 @@
-// This package provides a hybrid of MQ and WebSockets server with
-// support for horizontal scalability.
+// This package contains a binding for the `libuuid` - 
+// http://linux.die.net/man/3/libuuid.
 //
 // Copyright (C) 2011 by Krzysztof Kowalik <chris@nu7hat.ch>
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program. If not, see <http://www.gnu.org/licenses/>.
-package webrocket
+package uuid
 
 //#include <stdlib.h>
 //#include <uuid.h>
