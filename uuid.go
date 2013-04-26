a8beec139bb52ebf7b4b3282f83dd48774f2a3a1
Sergey Shepelev <temotor@gmail.com>
2013-04-26 17:44:34 +0400

    gofmt

--- uuid.go
+++ uuid.go
@@ -6,17 +6,17 @@
 package uuid
 
 import (
-	"errors"
-	"crypto/rand"
 	"crypto/md5"
+	"crypto/rand"
 	"crypto/sha1"
 	"encoding/hex"
-	"regexp"
-	"hash"
+	"errors"
 	"fmt"
+	"hash"
+	"regexp"
 )
 
-// The UUID reserved variants. 
+// The UUID reserved variants.
 const (
 	ReservedNCS       byte = 0x80
 	ReservedRFC4122   byte = 0x40
@@ -29,15 +29,14 @@ var (
 	NamespaceDNS, _  = ParseHex("6ba7b810-9dad-11d1-80b4-00c04fd430c8")
 	NamespaceURL, _  = ParseHex("6ba7b811-9dad-11d1-80b4-00c04fd430c8")
 	NamespaceOID, _  = ParseHex("6ba7b812-9dad-11d1-80b4-00c04fd430c8")
-	NamespaceX500, _ = ParseHex("6ba7b814-9dad-11d1-80b4-00c04fd430c8")	
+	NamespaceX500, _ = ParseHex("6ba7b814-9dad-11d1-80b4-00c04fd430c8")
 )
 
 // Pattern used to parse hex string representation of the UUID.
 // FIXME: do something to consider both brackets at one time,
 // current one allows to parse string with only one opening
 // or closing bracket.
-const hexPattern =
-	"^(urn\\:uuid\\:)?\\{?([a-z0-9]{8})-([a-z0-9]{4})-" +
+const hexPattern = "^(urn\\:uuid\\:)?\\{?([a-z0-9]{8})-([a-z0-9]{4})-" +
 	"([1-5][a-z0-9]{3})-([a-z0-9]{4})-([a-z0-9]{12})\\}?$"
 
 // A UUID representation copmliant with specification in
@@ -66,14 +65,14 @@ func ParseHex(s string) (u *UUID, err error) {
 	}
 	u = new(UUID)
 	copy(u[:], b)
-	return 
+	return
 }
 
 // Parse creates a UUID object from given bytes slice.
 func Parse(b []byte) (u *UUID, err error) {
 	if len(b) != 16 {
 		err = errors.New("Given slice is not valid UUID sequence")
-		return 
+		return
 	}
 	u = new(UUID)
 	copy(u[:], b)
@@ -125,7 +124,7 @@ func NewV5(ns *UUID, name []byte) (u *UUID, err error) {
 func (u *UUID) setBytesFromHash(hash hash.Hash, ns, name []byte) {
 	hash.Write(ns[:])
 	hash.Write(name)
-	copy(u[:], hash.Sum([]byte{})[:16])
+	copy(u[:], hash.Sum(nil)[:16])
 }
 
 // Set the two most significant bits (bits 6 and 7) of the
@@ -145,13 +144,13 @@ func (u *UUID) setVariant(v byte) {
 // layout of the UUID. This will be one of the constants: RESERVED_NCS,
 // RFC_4122, RESERVED_MICROSOFT, RESERVED_FUTURE.
 func (u *UUID) Variant() byte {
-	if u[8] & ReservedNCS == ReservedNCS {
+	if u[8]&ReservedNCS == ReservedNCS {
 		return ReservedNCS
-	} else if u[8] & ReservedRFC4122 == ReservedRFC4122 {
+	} else if u[8]&ReservedRFC4122 == ReservedRFC4122 {
 		return ReservedRFC4122
-	} else if u[8] & ReservedMicrosoft == ReservedMicrosoft {
+	} else if u[8]&ReservedMicrosoft == ReservedMicrosoft {
 		return ReservedMicrosoft
-	} 
+	}
 	return ReservedFuture
 }
 
